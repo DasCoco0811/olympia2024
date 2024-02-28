d7ae05e8e10c3ff497fb5029f3d31a1aa4ceb581
DasCoco <c.nagy@bui-hinsche.com>
2024-02-28 14:16:32 +0100

    - dashboard und so und api is ready immernoch

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -9,6 +9,7 @@ use App\Entity\TimesShowjumping;
 use App\Entity\TimesSprint;
 use App\Entity\TimesSwimming;
 use App\Entity\User;
+use App\Repository\AthletesRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -17,25 +18,41 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractDashboardController
 {
+    private AthletesRepository $athletesRepository;
+
+    public function __construct(AthletesRepository $athletesRepository)
+    {
+        $this->athletesRepository = $athletesRepository;
+    }
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-
-        return $this->render('admin/index.html.twig');
+        $athletes = $this->athletesRepository->getApiAllAthletes();
+        return $this->render('admin/index.html.twig', [
+            'athletes' => $athletes
+        ]);
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Olympia Paris 2024');
+            ->setTitle('Olympia Paris 2024')
+            ->setFaviconPath('favicon.ico')
+            ;
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::section("Account Management");
         yield MenuItem::linkToCrud('Accounts', 'fas fa-user', User::class);
+
+        yield MenuItem::section("Athlete Options");
         yield MenuItem::linkToCrud('Athletes', 'fas fa-list', Athletes::class);
         yield MenuItem::linkToCrud('Countries', 'fas fa-list', Countries::class);
+
+        yield MenuItem::section("Times");
         yield MenuItem::linkToCrud('Swimming Times', 'fas fa-list', TimesSwimming::class);
         yield MenuItem::linkToCrud('Sprint Times', 'fas fa-list', TimesSprint::class);
         yield MenuItem::linkToCrud('Showjumping Times', 'fas fa-list', TimesShowjumping::class);
