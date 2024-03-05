7abd681cee2e701af00a846a23e8b49e929eab62
Shukaaa <asappokimane@gmail.com>
2024-03-05 18:29:03 +0100

    Added cors

--- public/index.php
+++ public/index.php
@@ -1,9 +1,18 @@
 <?php
-
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
+    $allowedOriginsString = $context['ALLOWED_ORIGINS'] ?? '';
+
+    header('Access-Control-Allow-Origin:*');
+    header('Access-Control-Allow-Headers:*');
+    header('Access-Control-Allow-Credentials:true');
+    header('Access-Control-Allow-Headers:X-Requested-With, Content-Type, withCredentials');
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        die();
+    }
+
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
