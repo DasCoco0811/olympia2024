32f7d1e74f61edaa74a90f8465def0179aa4b793
Shukaaa <asappokimane@gmail.com>
2024-03-05 20:25:33 +0100

    Cooles Dashboard hehe

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -10,6 +10,8 @@ use App\Entity\TimesSprint;
 use App\Entity\TimesSwimming;
 use App\Entity\User;
 use App\Repository\AthletesRepository;
+use App\Repository\CountriesRepository;
+use App\Repository\SportsRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -19,18 +21,36 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractDashboardController
 {
     private AthletesRepository $athletesRepository;
+    private CountriesRepository $countriesRepository;
+    private SportsRepository $sportsRepository;
 
-    public function __construct(AthletesRepository $athletesRepository)
-    {
+    public function __construct(
+        AthletesRepository $athletesRepository,
+        CountriesRepository $countriesRepository,
+        SportsRepository $sportsRepository
+    ) {
         $this->athletesRepository = $athletesRepository;
+        $this->countriesRepository = $countriesRepository;
+        $this->sportsRepository = $sportsRepository;
     }
 
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
         $athletes = $this->athletesRepository->getApiAllAthletes();
+        $countries = $this->countriesRepository->getApiAllCountries();
+        $longjump = $this->sportsRepository->getApiLongJump();
+        $showjumping = $this->sportsRepository->getApiShowJumping();
+        $sprint = $this->sportsRepository->getApiSprint();
+        $swimming = $this->sportsRepository->getApiSwimming();
+
         return $this->render('admin/index.html.twig', [
-            'athletes' => $athletes
+            'athletes' => $athletes,
+            'countries' => $countries,
+            'longjump' => $longjump,
+            'showjumping' => $showjumping,
+            'sprint' => $sprint,
+            'swimming' => $swimming
         ]);
     }
 
